678ec372a3400e3cec193983d16e6716e45b7a7e
kovacsbalazspeter21 <71692659+kovacsbalazspeter21@users.noreply.github.com>
2020-11-03 09:32:11 +0100

    Add files via upload

---
+++ f0003.py
@@ -0,0 +1,4 @@
+##Ebből a posztból azt tudod meg, hogy miként tehető a Python-programod kíváncsivá. Íme a teljes program:
+
+név = input('Hogy hívnak téged? ')
+print('Szervusz ', név, ', örülök, hogy találkoztunk.', sep='')
